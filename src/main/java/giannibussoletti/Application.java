3e0d94989e19b0d7af085e5b7da61f128baad91a
giannibussoletti <bussolettigianni@gmail.com>
2026-06-22 15:24:08 +0200

    Creato EntityManagerFactory
    nome del database "bw4traspublic"

--- src/main/java/giannibussoletti/Application.java
+++ src/main/java/giannibussoletti/Application.java
@@ -1,8 +1,15 @@
 package giannibussoletti;
 
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Application {
 
+    public static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("bw4traspublicpu");
+
     public static void main(String[] args) {
+
+
         System.out.println("Hello World!");
     }
 }
