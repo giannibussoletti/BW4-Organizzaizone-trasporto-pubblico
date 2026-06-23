cf33edbacc46de45ba1dab5ad4ecfe810ca44d2e
giannibussoletti <bussolettigianni@gmail.com>
2026-06-23 12:46:02 +0200

    creazione dei save di base

--- src/main/java/DAO/AbbonamentoDAO.java
+++ src/main/java/DAO/AbbonamentoDAO.java
@@ -1,4 +1,26 @@
 package DAO;
 
+import entities.Abbonamento;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+
 public class AbbonamentoDAO {
+
+    private final EntityManager entityManager;
+
+    public AbbonamentoDAO(EntityManager em) {
+        this.entityManager = em;
+    }
+
+    public void save(Abbonamento newAbbonamento) {
+        EntityTransaction transaction = this.entityManager.getTransaction();
+
+        transaction.begin();
+
+        this.entityManager.persist(newAbbonamento);
+
+        transaction.commit();
+
+        System.out.println("Nuovo Abbonamento salvato correttamente");
+    }
 }
--- src/main/java/DAO/PuntoEmissioneDAO.java
+++ src/main/java/DAO/PuntoEmissioneDAO.java
@@ -1,4 +1,26 @@
 package DAO;
 
+import entities.PuntoEmissione;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+
 public class PuntoEmissioneDAO {
+
+    private final EntityManager entityManager;
+
+    public PuntoEmissioneDAO(EntityManager em) {
+        this.entityManager = em;
+    }
+
+    public void save(PuntoEmissione newPuntoEmissione) {
+        EntityTransaction transaction = this.entityManager.getTransaction();
+
+        transaction.begin();
+
+        this.entityManager.persist(newPuntoEmissione);
+
+        transaction.commit();
+
+        System.out.println("Punto di emissione creato correttamente");
+    }
 }
