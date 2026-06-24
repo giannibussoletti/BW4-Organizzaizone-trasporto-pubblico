32028647106c4cef79a5cfa8b9999e97c81fa10e
Daniele Bulgaru <daniele.bulgaru10@gmail.com>
2026-06-24 15:38:20 +0200

    TrattaDao/PercorrenzaDAO

--- src/main/java/DAO/PercorrenzaDAO.java
+++ src/main/java/DAO/PercorrenzaDAO.java
@@ -1,4 +1,26 @@
 package DAO;
 
+import entities.Percorrenza;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+
 public class PercorrenzaDAO {
+
+    private final EntityManager entityManager;
+
+    public PercorrenzaDAO(EntityManager em) {
+        this.entityManager = em;
+    }
+
+    public void save(Percorrenza newPercorrenza) {
+        EntityTransaction transaction = this.entityManager.getTransaction();
+
+        transaction.begin();
+
+        this.entityManager.persist(newPercorrenza);
+
+        transaction.commit();
+
+        System.out.println("Percorrenza creata correttamente");
+    }
 }
--- src/main/java/DAO/TrattaDAO.java
+++ src/main/java/DAO/TrattaDAO.java
@@ -1,4 +1,26 @@
 package DAO;
 
+import entities.Tratta;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+
 public class TrattaDAO {
+
+    private final EntityManager entityManager;
+
+    public TrattaDAO(EntityManager em) {
+        this.entityManager = em;
+    }
+
+    public void save(Tratta newTratta) {
+        EntityTransaction transaction = this.entityManager.getTransaction();
+
+        transaction.begin();
+
+        this.entityManager.persist(newTratta);
+
+        transaction.commit();
+
+        System.out.println("Tratta creata correttamente");
+    }
 }
